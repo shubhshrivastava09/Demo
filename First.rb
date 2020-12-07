d51a651bd9c9d5eef7bddd74813a023a561be08b
shubhshrivastava09 <75485937+shubhshrivastava09@users.noreply.github.com>
2020-12-07 14:51:19 +0530

    Create First.rb
    FIrst Program

---
+++ First.rb
@@ -0,0 +1,13 @@
+
+
+email = "rahul sain@ gmail.com"
+str = "abbababababdcbabab"
+st ='ab'
+rahu="   doraemon  "
+puts str.count(st)
+
+puts st.insert( 0,"cdefgh")
+ puts email.split()
+puts rahu.strip
+puts "H".to_i
+puts st.clear
